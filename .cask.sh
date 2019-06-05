858306acd1226d52a83ff3ff1b49ea6d8c0e4f1f
Kentoy Johannessen <jankenith.johannessen@gmail.com>
2019-06-05 22:34:55 +0800

    Initial commit of dotfiles

---
+++ .cask.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Make sure we update homebrew
+brew update
+
+# Upgrade necessary tools
+brew upgrade
+
+# Development
+brew cask install docker
+
+# Browsers
+brew cask install firefox
+brew cask install google-chrome
+# brew cask install brave-browser
+
+# Install utils app
+# brew cask install dash
+brew cask install insomnia
+brew cask install sequel-pro
+# brew cask install keybase
+# brew cask install amethyst
+
+# Install productivity
+brew cask install slack
+brew cask install spotify
+
+# Others
+brew cask install transmission
+brew cask install iina
+brew cask install caprine
+# brew cask install vlc
+
+
+# Remove outdated versions from the cellar.
+brew cleanup
